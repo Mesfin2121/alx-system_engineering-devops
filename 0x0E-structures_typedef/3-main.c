4e15d0665be2e7bef4f6739dce51134748beaff4
Mesfin2121 <medmesfin20@gmail.com>
2022-12-18 02:06:41 -0800

    Task 15

--- 0x0E-structures_typedef/3-main.c
+++ 0x0E-structures_typedef/3-main.c
@@ -3,7 +3,6 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
@@ -13,6 +12,6 @@ int main(void)
 	my_dog.name = "Poppy";
 	my_dog.age = 3.5;
 	my_dog.owner = "Bob";
-	print_dog(&my_dog);
+	print_dog("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 	return (0);
 }
